b728364671a4ad520c8b4bfb1cd09390a35097ab
prinonbhowmik <prinonarnob@gmail.com>
2025-05-13 01:59:39 +0600

    flexbox

--- FirstProject/App.js
+++ FirstProject/App.js
@@ -4,16 +4,16 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <View>
+      <View style={styles.inputContainer}>
         <TextInput
-          style={styles.textD}
+          style={styles.textInput}
           placeholder='Your goal' />
         <Button
           backgroundColor='red'
           title='Add Goal'
         />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals....</Text>
       </View>
     </View>
@@ -23,16 +23,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 16
   },
-  textD: {
-    margin: 16,
-    borderWidth: 2,
+  textInput: {
+    borderWidth: 1,
     borderColor: 'black',
     padding: 10,
-    width: '80%',
+    width: '70%',
+    marginRight: 8,
+    padding: 8
   },
-
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: 'black'
+  },
+  goalsContainer: {
+    flex: 5
+  }
 });
